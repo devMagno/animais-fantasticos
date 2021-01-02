5fe361a9d7a6186056994768142b34154fa3ddbd
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-01-02 15:20:21 -0300

    Adiciona função da lista de accordion FAQ

--- script.js
+++ script.js
@@ -17,3 +17,20 @@ function initTabNav() {
 }
 
 initTabNav()
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt')
+  if (accordionList.length) {
+    accordionList[0].classList.add('ativo')
+    accordionList[0].nextElementSibling.classList.add('ativo')
+  
+    function activeAccordion(event) {
+      this.classList.toggle('ativo')
+      this.nextElementSibling.classList.toggle('ativo')
+    }
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion)
+    })  
+  }
+}
+initAccordion()
