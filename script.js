b6ca79583b23e0ad5027db66ae69d96a3fe691c0
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-01-02 15:29:36 -0300

    Refatora adição de classes

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+const activeClass = 'ativo'
 function initTabNav() {
   const tabMenu = document.querySelectorAll('.js-tabmenu li')
   const tabContent = document.querySelectorAll('.js-tabcontent section')
   if(tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo')
+    tabContent[0].classList.add(activeClass)
 
     function activeTab(index) {
       tabContent.forEach((section) => {
-        section.classList.remove('ativo')
+        section.classList.remove(activeClass)
       })
-      tabContent[index].classList.add('ativo')
+      tabContent[index].classList.add(activeClass)
     }
     tabMenu.forEach((itemMenu, index) => {
       itemMenu.addEventListener('click', () => activeTab(index))
@@ -21,12 +22,12 @@ initTabNav()
 function initAccordion() {
   const accordionList = document.querySelectorAll('.js-accordion dt')
   if (accordionList.length) {
-    accordionList[0].classList.add('ativo')
-    accordionList[0].nextElementSibling.classList.add('ativo')
+    accordionList[0].classList.add(activeClass)
+    accordionList[0].nextElementSibling.classList.add(activeClass)
   
     function activeAccordion(event) {
-      this.classList.toggle('ativo')
-      this.nextElementSibling.classList.toggle('ativo')
+      this.classList.toggle(activeClass)
+      this.nextElementSibling.classList.toggle(activeClass)
     }
     accordionList.forEach((item) => {
       item.addEventListener('click', activeAccordion)
