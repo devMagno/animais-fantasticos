c8fc8a035a8706045ea93db7827404e2a09c98ab
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-01-02 21:24:22 -0300

    Adiciona script do scroll suave para links internos

--- script.js
+++ script.js
@@ -35,3 +35,22 @@ function initAccordion() {
   }
 }
 initAccordion()
+
+function initSmoothScroll() {
+  const internalAnchors = document.querySelectorAll('.js-menu a[href^="#"]')
+  
+  function scrollToSection(event) {
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute('href')
+    const section = document.querySelector(href)
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  }
+  
+  internalAnchors.forEach((item) => {
+    item.addEventListener('click', scrollToSection)
+  })
+}
+initSmoothScroll()
