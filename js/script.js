908cc5600c94e4907596dbb9ea856b2c5fdf6e63
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-04-28 23:49:29 -0300

    Adiciona scripts do modal

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initSmoothScroll from './modules/smooth-scroll.js'
 import initAnimateOnScroll from './modules/animate-on-scroll.js'
 import initAccordion from "./modules/accordion.js"
 import initTabNav from "./modules/tab-nav.js";
+import initModal from "./modules/modal.js";
 
 initSmoothScroll()
 initAnimateOnScroll()
 initAccordion()
 initTabNav()
+initModal()
