af42bc01998858d54fc296112249cce621ae8b59
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-04-29 21:07:10 -0300

    Adiciona tooltip

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAnimateOnScroll from './modules/animate-on-scroll.js'
 import initAccordion from "./modules/accordion.js"
 import initTabNav from "./modules/tab-nav.js";
 import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
 
 initSmoothScroll()
 initAnimateOnScroll()
 initAccordion()
 initTabNav()
 initModal()
+initTooltip()
