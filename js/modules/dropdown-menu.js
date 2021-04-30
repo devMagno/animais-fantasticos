59c3f63c6645fe6e0dc5ba84accd271c61c21dac
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-04-29 23:58:56 -0300

    Adiciona scripts do menu dropdown

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,3 +1,19 @@
-export default function initDropdownMenu() {
+import outsideClick from "./outside-click.js";
 
+export default function initDropdownMenu() {
+  const dropdownMenus = document.querySelectorAll('[data-dropdown]')
+  
+  dropdownMenus.forEach(menu => {
+    ['touchstart', 'click'].forEach(userEvent => {
+      menu.addEventListener(userEvent, handleClick)
+    })
+  })
+  
+  function handleClick(event) {
+    event.preventDefault()
+    this.classList.add('ativo')
+    outsideClick(this, ['touchstart', 'click'], () => {
+      this.classList.remove('ativo')
+    })
+  }
 }
---
+++ js/modules/outside-click.js
@@ -0,0 +1,20 @@
+export default function outsideClick(element, events, callback) {
+  const outside = 'data-outside'
+  const html = document.documentElement
+
+  if(!element.hasAttribute(outside)) {
+    events.forEach(userEvent => {
+      html.addEventListener(userEvent, handleOutsideClick)
+    })
+    element.setAttribute(outside, '')
+  }
+  function handleOutsideClick(event) {
+    if(!element.contains(event.target)) {
+      events.forEach(userEvent => {
+        html.removeEventListener(userEvent, handleOutsideClick)
+      })
+      element.removeAttribute(outside)
+      callback()
+    }
+  }
+}
