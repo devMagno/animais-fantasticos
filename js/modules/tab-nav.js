facecd755b52208a94bbd7850d092c69a8482622
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-06-01 15:07:44 -0300

    Refatora tab nav utilizando classe

--- js/modules/tab-nav.js
+++ js/modules/tab-nav.js
@@ -1,17 +1,44 @@
-export default function initTabNav() {
-  const tabMenu = document.querySelectorAll('[data-tab="menu"] li')
-  const tabContent = document.querySelectorAll('[data-tab="content"] section')
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove('ativo')
-    })
-    tabContent[index].classList.add('ativo', tabContent[index].dataset.anime)
-  }
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo')
+// function activeTab(index) {
+//   tabContent.forEach((section) => {
+//     section.classList.remove(this.activeClass)
+//   })
+//   tabContent[index].classList.add(this.activeClass, tabContent[index].dataset.anime)
+// }
+// if (tabMenu.length && tabContent.length) {
+//   tabContent[0].classList.add(this.activeClass)
 
-    tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener('click', () => activeTab(index))
+//   tabMenu.forEach((itemMenu, index) => {
+//     itemMenu.addEventListener('click', () => activeTab(index))
+//   })
+// }
+
+export default class tabNav {
+  constructor(menu, content) {
+    this.tabMenu = document.querySelectorAll(menu)
+    this.tabContent = document.querySelectorAll(content)
+    this.activeClass = 'ativo'
+  }
+
+  activateTab(index) {
+    this.tabContent.forEach((section) => {
+      section.classList.remove(this.activeClass)
     })
+    this.tabContent[index].classList.add(
+      this.activeClass,
+      this.tabContent[index].dataset.anime
+    )
+  }
+
+  addTabMenuEvent() {
+    this.tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => this.activateTab(index))
+    })
+  }
+
+  init() {
+    if (this.tabMenu.length && this.tabContent.length) {
+      this.addTabMenuEvent()
+      this.activateTab(0)
+    }
   }
 }
