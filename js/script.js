75c75cafa3721ed348d5bf3dbce2c51896830f84
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-06-01 14:26:48 -0300

    Refatora acordeão utilizando classe

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import SmoothScroll from './modules/smooth-scroll.js'
 import initAnimateOnScroll from './modules/animate-on-scroll.js'
-import initAccordion from './modules/accordion.js'
+import Accordion from './modules/accordion.js'
 import initTabNav from './modules/tab-nav.js'
 import initModal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js'
@@ -12,8 +12,11 @@ import initFetchBitcoin from './modules/fetch-bitcoin.js'
 
 const smoothScroll = new SmoothScroll('[data-menu="smooth"] a[href^="#"')
 smoothScroll.init()
+
+const accordion = new Accordion('[data-anime="accordion"] dt')
+accordion.init()
+
 initAnimateOnScroll()
-initAccordion()
 initTabNav()
 initModal()
 initTooltip()
