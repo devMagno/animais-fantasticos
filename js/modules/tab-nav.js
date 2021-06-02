1f8b3c01bc07ff1a0c0abd33b9bfba729ea3903c
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-06-01 23:35:49 -0300

    Remove comentários

--- js/modules/tab-nav.js
+++ js/modules/tab-nav.js
@@ -1,17 +1,3 @@
-// function activeTab(index) {
-//   tabContent.forEach((section) => {
-//     section.classList.remove(this.activeClass)
-//   })
-//   tabContent[index].classList.add(this.activeClass, tabContent[index].dataset.anime)
-// }
-// if (tabMenu.length && tabContent.length) {
-//   tabContent[0].classList.add(this.activeClass)
-
-//   tabMenu.forEach((itemMenu, index) => {
-//     itemMenu.addEventListener('click', () => activeTab(index))
-//   })
-// }
-
 export default class tabNav {
   constructor(menu, content) {
     this.tabMenu = document.querySelectorAll(menu)
