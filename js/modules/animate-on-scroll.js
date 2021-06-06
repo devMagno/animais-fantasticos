b28be1093911c5328ed239054012ec156dca1745
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-06-06 12:52:58 -0300

    Adiciona debounce na animação ao scroll

--- js/modules/animate-on-scroll.js
+++ js/modules/animate-on-scroll.js
@@ -1,9 +1,11 @@
+import debounce from './debounce.js'
+
 export default class AnimateOnScroll {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections)
     this.halfScreen = window.innerHeight * 0.6
 
-    this.checkDistance = this.checkDistance.bind(this)
+    this.checkDistance = debounce(this.checkDistance.bind(this), 200)
   }
 
   getDistance() {
