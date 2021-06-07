a5a98836ae01a82d1f8d86d8cbf9fdd008b5adbb
Guilherme Magno <guilhermertfmagno@gmail.com>
2021-06-07 09:55:35 -0300

    Refatora menu mobile utilizando classe

--- js/modules/mobile-menu.js
+++ js/modules/mobile-menu.js
@@ -1,21 +1,37 @@
 import outsideClick from './outside-click.js'
 
-export default function initMobileMenu() {
-  const events = ['click', 'touchstart']
-  const menuButton = document.querySelector('[data-menu="button"]')
-  const menuList = document.querySelector('[data-menu="list"]')
+export default class MobileMenu {
+  constructor(menuButton, menuList, events) {
+    this.menuButton = document.querySelector(menuButton)
+    this.menuList = document.querySelector(menuList)
+    if (events === undefined) {
+      this.events = ['touchstart', 'click']
+    } else {
+      this.events = events
+    }
+    this.activeClass = 'active'
+    this.openMenu = this.openMenu.bind(this)
+  }
 
-  function openMenu() {
-    menuButton.classList.add('active')
-    menuList.classList.add('active')
-    outsideClick(menuList, ['click', 'touch-start'], () => {
-      menuButton.classList.remove('active')
-      menuList.classList.remove('active')
+  openMenu() {
+    this.menuButton.classList.add(this.activeClass)
+    this.menuList.classList.add(this.activeClass)
+    outsideClick(this.menuList, this.events, () => {
+      this.menuButton.classList.remove(this.activeClass)
+      this.menuList.classList.remove(this.activeClass)
     })
   }
-  if (menuButton) {
-    events.forEach((userEvent) =>
-      menuButton.addEventListener(userEvent, openMenu)
+
+  addMobileMenuEvents() {
+    this.events.forEach((userEvent) =>
+      this.menuButton.addEventListener(userEvent, this.openMenu)
     )
+  }
+
+  init() {
+    if (this.menuButton && this.menuList) {
+      this.addMobileMenuEvents()
+    }
+    return this
   }
 }
--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ import TabNav from './modules/tab-nav.js'
 import Modal from './modules/modal.js'
 import Tooltip from './modules/tooltip.js'
 import DropdownMenu from './modules/dropdown-menu.js'
-import initMobileMenu from './modules/mobile-menu.js'
+import MobileMenu from './modules/mobile-menu.js'
 import initOpeningHours from './modules/opening-hours.js'
 import fetchAnimals from './modules/fetch-animals.js'
 import fetchBitcoin from './modules/fetch-bitcoin.js'
@@ -38,7 +38,9 @@ animateOnScroll.init()
 const dropdownMenu = new DropdownMenu('[data-dropdown]')
 dropdownMenu.init()
 
-initMobileMenu()
+const mobileMenu = new MobileMenu('[data-menu="button"]', '[data-menu="list"]')
+mobileMenu.init()
+
 initOpeningHours()
 
 fetchAnimals('../../animalsAPI.json', '.numeros-grid')
